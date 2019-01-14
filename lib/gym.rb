77c00be6bb0ae3ae9dffa38dabb5003ac5ff5984
James Clement <jameseclement@gmail.com>
2019-01-14 15:13:59 -0500

    first attempt

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,32 @@
 class Gym
   attr_reader :name
 
+@@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership|
+       membership.gym == self}
+  end
+
+  def lifters
+    self.memberships.map {|membership| membership.lifter}
+  end
+
+  def lifters_names
+    self.lifters.map{|lifter| lifter.name}
+  end
+
+  def total_weight
+  totals = self.lifters.map {|lifter| lifter.lift_total}
+  totals.inject{|x,y| x+y }
   end
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,42 @@
 class Lifter
-  attr_reader :name, :lift_total
+
+  attr_reader :name, :lift_total, :memberships
+
+@@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+def self.all
+  @@all
+end
+
+def self.avg_lift_total
+  totals = Lifter.all.map {|lifter| lifter.lift_total}
+  totals.inject{|sum, el| sum + el }.to_f / totals.size
+end
+
+
+def memberships
+  Membership.all.select {|membership|
+     membership.lifter == self}
+end
+
+def gyms
+self.memberships.map {|membership| membership.gym}
+end
+
+def total_cost
+  costs= self.memberships.map{|membership| membership.cost}
+  costs.inject {|x, y| x + y}
+end
+
+def sign_up (gym, membership_cost)
+  Membership.new(membership_cost, self, gym)
+
+end
+
 end
